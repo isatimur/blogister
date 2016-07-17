b394d6c3ac3b6204930b57d01bed89c6ae9b3707
Timur Isa <tisachenko@at-consulting.ru>
2016-07-17 17:55:47 +0300

    added richtext - textAngularr module + ngSanitize module

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -16,7 +16,9 @@
             'ui.router',
             'infinite-scroll',
             // jhipster-needle-angularjs-add-module JHipster will add new module here
-            'angular-loading-bar'
+            'angular-loading-bar',
+            'ngSanitize',
+            'textAngular'
         ])
         .run(run);
 
